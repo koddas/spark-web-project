509d43e5dd7431b0f87acb4165038df9855b84d0
koddas <johan@idioti.se>
2015-02-03 14:13:12 +0100

    Implemented the web service.

---
+++ readme.md
@@ -0,0 +1,4 @@
+# About this project
+
+This project is a simple demo web application using the Spark framework. It
+runs on Java 8 and requires the Spark framework in order to run.
---
+++ src/main/java/koddas/web/spark/WebService.java
@@ -0,0 +1,41 @@
+package koddas.web.spark;
+
+import static spark.Spark.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * A very simple web service.
+ * 
+ * @author Johan Holmberg
+ */
+public class WebService {
+
+	/**
+	 * Implicitly sets up a web server and publishes a set of endpoints that
+	 * can be accessed through HTTP.
+	 * 
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args) {
+		setPort(8080);
+		
+		// A very simple Hello World end point
+		get("/hello", (req, res) -> "Hello, World!");
+		
+		// A demonstration of how to use code within an endpoint
+		get("/time", (req, res) -> {
+			DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+			Date dateobj = new Date();
+			
+			return df.format(dateobj);
+		});
+		
+		post("/send", (req, res) -> {
+			return "You sent me\n" + req.body() + "\nfrom " + req.ip() + "\n\n Thank you!";
+		});
+	}
+
+}
